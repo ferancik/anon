a4c0c2aa941cefcc575db22058ea761ae68ab7b7
KFK GROUP s. r. o. <ferancik@orava.sk>
2016-04-15 14:13:12 +0200

    1. verzia webu

--- application/views/themes/metronic/template.php
+++ application/views/themes/metronic/template.php
@@ -6,7 +6,7 @@
 <html lang="en">
     <!--<![endif]-->
     <!-- BEGIN HEAD -->
-
+<!-- zmena -->
     <head>
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
